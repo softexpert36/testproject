cb87965f3ed3651ffa180ca90edf78a963492ed3
softexpert36 <mathanexpert@yahoo.com>
2019-10-25 10:50:24 -0400

    inital commit

---
+++ TestProject/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TestProject.Model;
+namespace TestProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        public ActionResult getDTO()
+        {
+            string res = new DTO().getDTO();
+            return Content(res, "application/json");
+        }
+
+    }
+}
